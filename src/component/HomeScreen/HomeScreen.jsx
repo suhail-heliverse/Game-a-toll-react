5783580a86264b7b74187d10d6614499a4fed8fd
suhail-heliverse <suhail@heliverse.com>
2021-11-08 14:49:13 +0530

    if a match is active then only show the match on home screen (fixed)

--- src/component/HomeScreen/HomeScreen.jsx
+++ src/component/HomeScreen/HomeScreen.jsx
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { fetchData } from "../../middleware/RequestHandler";
 import { Footer, Faq, Banner, Slider, Header , HowPlay} from "../index";
+import VisitorsView from "../VisitorsView/VisitorsView";
 import "./HomeScreen.css"
 
 function HomeScreen() {
   const [showPopup, setShowPopup] = useState(false);
+  const [currentMatchStatus, setCurrentMatchStatus] = useState(false);
+  useEffect(async () => {
+    const response = await fetchData("/getActiveMatch", { method: "GET" });
+    if (response.status) {
+      setCurrentMatchStatus(true);
+    }
+  }, []);
+
   function gamePopup() {
     setShowPopup(!showPopup);
   }
@@ -14,6 +24,7 @@ function HomeScreen() {
         <Header gamePopup={gamePopup} />
         {/* {showPopup && <GamePopup />} */}
         <Banner />
+        {currentMatchStatus && <VisitorsView callback={()=>{}}/>}
         <HowPlay />
         <Slider />
         <Faq />
